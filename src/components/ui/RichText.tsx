6bbda996ecfeb3e4af6bb00397919b004f86e64d
kareraolivier <kareraolivier@gmail.com>
2025-07-14 20:19:35 +0200

    feat: update richtext

--- src/components/ui/RichText.tsx
+++ src/components/ui/RichText.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import {
   BlocksRenderer,
   type BlocksContent,
@@ -23,33 +23,13 @@ const RichText = ({ content, isHero = false }: RichTextType) => {
           ),
 
           paragraph: ({ children }) => {
-            const targetText =
-              "digital solutions that make the world a better place.";
-
-            // Convert all children to string (in case they're nested)
-            const content = React.Children.toArray(children);
-
-            const highlightedContent = content.map((child, index) => {
-              if (typeof child === "string" && child.includes(targetText)) {
-                const [before, after] = child.split(targetText);
-                return (
-                  <React.Fragment key={index}>
-                    {before}
-                    <span className="bg-primary">{targetText}</span>
-                    {after}
-                  </React.Fragment>
-                );
-              }
-              return child;
-            });
-
             return (
               <div
                 className={`${
-                  isHero ? "text-white" : ""
-                } text-2xl lg:text-3xl font-normal leading-7.5 lg:leading-11 tracking-normal`}
+                  isHero && "text-white"
+                } text-2xl  lg:text-3xl font-normal leading-7.5 lg:leading-11 tracking-normal`}
               >
-                {highlightedContent}
+                {children}
               </div>
             );
           },
