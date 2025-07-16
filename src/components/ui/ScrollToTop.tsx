698be2466e824f5949785e7a0a8428edf138e050
kareraolivier <kareraolivier@gmail.com>
2025-07-16 11:00:05 +0200

    feat: remove hovered colors

--- src/components/ui/ScrollToTop.tsx
+++ src/components/ui/ScrollToTop.tsx
@@ -21,7 +21,7 @@ const ScrollToTop = () => {
   return isVisible ? (
     <button
       onClick={scrollToTop}
-      className="fixed bottom-6 right-6 z-50 p-3 rounded-md bg-primary hover:bg-primary/20 transition shadow-lg"
+      className="fixed bottom-6 right-6 z-50 p-3 rounded-md bg-primary transition shadow-lg"
       aria-label="Scroll to top"
     >
       <Image src="/up-arrow.svg" alt="Up-arrow" width={24} height={24} />
