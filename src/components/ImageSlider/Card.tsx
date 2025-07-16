41d378e56eb82e51fb32b3b56e3531020cb94a51
kareraolivier <kareraolivier@gmail.com>
2025-07-16 12:13:36 +0200

    feat: change image height

--- src/components/ImageSlider/Card.tsx
+++ src/components/ImageSlider/Card.tsx
@@ -10,12 +10,13 @@ const Card = ({ link, image }: { link: CardLink; image: CardImage }) => {
       target={link?.isExternal ? "_blank" : "_self"}
       rel="noopener noreferrer"
     >
-      <div className="relative mx-6 h-28 w-48 cursor-pointer">
+      <div className="relative mx-6 h-32 w-48 cursor-pointer">
         <Image
           src={image.url}
           alt={image.alternativeText + image.name + "image"}
           fill
           sizes="100%"
+          quality={100}
           className="object-contain"
           loading="eager"
         />
